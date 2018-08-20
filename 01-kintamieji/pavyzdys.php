9eb6037debed3fdbb27d0096a42f6fb8f8c14ae5
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-08-21 00:26:11 +0300

    programu pavyzdziai

---
+++ 01-kintamieji/pavyzdys.php
@@ -0,0 +1,13 @@
+<?php
+
+$marke = "porche";
+$modelis = "911";
+$metai = 2015;
+$rida = 15020;
+$darbinisTuris = 2.5;
+
+$vardas = "Jonas";
+$pavarde = "Jonaitis";
+$amzius = 59;
+$profesija = "mokytojas";
+$ugis = 172; // cm
