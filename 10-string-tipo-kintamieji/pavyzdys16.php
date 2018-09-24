c9cd042e037eef3ca57861ea32c8d6aeb576ed20
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-09-24 23:52:25 +0300

    programu pavyzdziai

--- 10-string-tipo-kintamieji/pavyzdys16.php
+++ 10-string-tipo-kintamieji/pavyzdys16.php
@@ -3,8 +3,18 @@
 /* substr_count(string, substring, start, length) */
 
 $str = "This is nice";
-echo strlen($str) . "<br>"; // Using strlen() to return the string length
-echo substr_count($str, "is") . "<br>"; // The number of times "is" occurs in the string
-echo substr_count($str, "is", 2) . "<br>"; // The string is now reduced to "is is nice"
-echo substr_count($str, "is", 3) . "<br>"; // The string is now reduced to "s is nice"
-echo substr_count($str, "is", 3, 3) . "<br>"; // The string is now reduced to "s i"
+
+// Using strlen() to return the string length
+echo strlen($str) . "<br>";
+
+// The number of times "is" occurs in the string
+echo substr_count($str, "is") . "<br>";
+
+// The string is now reduced to "is is nice"
+echo substr_count($str, "is", 2) . "<br>";
+
+// The string is now reduced to "s is nice"
+echo substr_count($str, "is", 3) . "<br>";
+
+// The string is now reduced to "s i"
+echo substr_count($str, "is", 3, 3) . "<br>";
