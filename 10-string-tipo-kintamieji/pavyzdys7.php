b5880817ce25e7792ca4dc5158170c6c178777c0
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-09-24 23:10:59 +0300

    programu pavyzdziai

--- 10-string-tipo-kintamieji/pavyzdys7.php
+++ 10-string-tipo-kintamieji/pavyzdys7.php
@@ -3,4 +3,5 @@
 $tekstas = "mano batai buvo du, vienas dingo, nerandu";
 $zodis = "buvo";
 
-echo "zodzio '$zodis' pozicija tekste '$tekstas' yra " . strpos($tekstas, $zodis);
+echo "zodzio '$zodis' pozicija tekste '$tekstas' yra "
+        . strpos($tekstas, $zodis);
