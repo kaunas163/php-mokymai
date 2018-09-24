908d88a963a77c7941ef84974c1fb971e3961d24
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-09-24 23:50:04 +0300

    programu pavyzdziai

--- 10-string-tipo-kintamieji/pavyzdys13.php
+++ 10-string-tipo-kintamieji/pavyzdys13.php
@@ -7,11 +7,14 @@ This function returns:
     > 0 - if string1 is greater than string2
 */
 
-echo strcmp("Hello world!", "Hello world!"); // the two strings are equal
+// the two strings are equal
+echo strcmp("Hello world!", "Hello world!");
 echo "<br>";
 
-echo strcmp("Hello world!", "Hello"); // string1 is greater than string2
+// string1 is greater than string2
+echo strcmp("Hello world!", "Hello");
 echo "<br>";
 
-echo strcmp("Hello world!", "Hello world! Hello!"); // string1 is less than string2
+// string1 is less than string2
+echo strcmp("Hello world!", "Hello world! Hello!");
 echo "<br>";
