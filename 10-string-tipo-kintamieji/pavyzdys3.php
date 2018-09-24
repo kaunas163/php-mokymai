bd003da45539cf39bc8f36fd71c2a7bfbe4fccdb
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2018-09-24 22:55:58 +0300

    programu pavyzdziai

---
+++ 10-string-tipo-kintamieji/pavyzdys3.php
@@ -0,0 +1,4 @@
+<?php
+
+$tekstas = "mano naujas tektas";
+echo "tekste '$tekstas' yra " . str_word_count($tekstas) . " zodziai";
---
+++ 10-string-tipo-kintamieji/pavyzdys7.php
@@ -0,0 +1,6 @@
+<?php
+
+$tekstas = "mano batai buvo du, vienas dingo, nerandu";
+$zodis = "buvo";
+
+echo "zodzio '$zodis' pozicija tekste '$tekstas' yra " . strpos($tekstas, $zodis);
